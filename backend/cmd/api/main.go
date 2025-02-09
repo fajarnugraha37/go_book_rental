8e1d492aead62e0eb9d6f59a337bdd8980c80c60
fajar <nugrahafajar37@gmail.com>
2025-02-09 17:33:24 +0700

    add fiber to main func (for test)

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"backend/internal/config"
 	"backend/internal/database"
-	"backend/internal/database/repo"
-	"backend/internal/database/repo/filter"
 	"backend/internal/logger"
-	"context"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
@@ -15,126 +15,32 @@ func main() {
 
 	cfg := config.LoadConfig()
 	db := database.GetBunDB(cfg)
-	userRepo := repo.NewUserRepo(db)
-	// userAttrRepo := repo.NewUserAttributeRepo(db)
+	app := fiber.New(fiber.Config{
+		ServerHeader:             cfg.App.Name,
+		AppName:                  cfg.App.Name,
+		CaseSensitive:            false,
+		StrictRouting:            true,
+		BodyLimit:                4 * 1024 * 1024,
+		Concurrency:              256 * 1024,
+		EnablePrintRoutes:        true,
+		EnableSplittingOnParsers: true,
+		WriteTimeout:             time.Second * 60,
+		ReadTimeout:              time.Second * 60,
+	})
 
-	// for i := 0; i < 15; i++ {
-	// 	u := &model.User{
-	// 		Username: "uname_" + strconv.Itoa(i) + time.Now().GoString(),
-	// 		IsActive: true,
-	// 	}
-	// 	userRepo.Insert(context.Background(), u)
+	app.All("/", func(c *fiber.Ctx) error {
+		result := db.QueryRow("SELECT current_timestamp")
+		var res any
+		result.Scan(&res)
+		return c.JSON(map[string]any{
+			"message": "Hello, World!",
+			"result":  res,
+		})
+	})
 
-	// 	userAttrRepo.Insert(context.Background(), &model.UserAttribute{
-	// 		UserID: u.ID,
-	// 		Key:    "first_name",
-	// 		Type:   "string",
-	// 		Value:  "first_name_" + strconv.Itoa(i),
-	// 	})
-	// 	userAttrRepo.Insert(context.Background(), &model.UserAttribute{
-	// 		UserID: u.ID,
-	// 		Key:    "last_name",
-	// 		Type:   "string",
-	// 		Value:  "last_name_" + strconv.Itoa(i),
-	// 	})
-	// 	userAttrRepo.Insert(context.Background(), &model.UserAttribute{
-	// 		UserID: u.ID,
-	// 		Key:    "business_name",
-	// 		Type:   "string",
-	// 		Value:  "business_name_" + strconv.Itoa(i),
-	// 	})
-	// }
+	app.All("/api", func(c *fiber.Ctx) error {
+		return c.SendString("API Hello, World!")
+	})
 
-	predicate := &filter.Predicate{
-		Operation: filter.OperationEnum("or"),
-		Filters: []filter.QueryFilter{
-			{
-				Field:      "users.is_active",
-				Comparator: filter.EQ,
-				Param:      true,
-			},
-		},
-		Pageable: &filter.Pageable{
-			Page:     1,
-			PageSize: 1,
-		},
-		Sortable: &[]filter.Sortable{
-			{
-				SortBy:    "users.created_at",
-				Direction: filter.DESC,
-			},
-		},
-		Relations: []string{"UserAttributes"},
-	}
-	page, err := userRepo.FindPageable(context.Background(), predicate)
-	log.Infof("Page %+v", page.Page)
-	log.Infof("TotalItems %+v", page.TotalItems)
-	log.Infof("page.TotalPages %+v", page.TotalPages)
-	log.Infof("Items %+v", page.Items[0].UserAttributes[0].Key)
-	log.Infof("Items %+v", page.Items[0].UserAttributes[0].Value)
-	log.Infof("error %+v\n\n", err)
-	// for {
-	// 	page, err := userRepo.FindPageable(context.Background(), predicate)
-	// 	log.Infof("Page %+v", page.Page)
-	// 	log.Infof("len(page.Items) %+v", len(page.Items))
-	// 	log.Infof("TotalItems %+v", page.TotalItems)
-	// 	log.Infof("page.TotalPages %+v", page.TotalPages)
-	// 	log.Infof("error %+v\n\n", err)
-	// 	if err == nil && page.HasNext() {
-	// 		predicate.Pageable.Page = page.NextPage
-	// 	} else {
-	// 		break
-	// 	}
-	// }
-
-	// user, err := ur.FindAll(
-	// 	context.Background(),
-	// 	repo.UserFilter{
-	// 		// Username: helper.ToPtr("%779_2"),
-	// 		CommonFilter: repo.CommonFilter{
-	// 			ID:        helper.ToPtr("0d81d764-23e0-4b8e-a08a-21df4f62ebda"),
-	// 			Relations: []string{"UserAttributes"},
-	// 		},
-	// 	},
-	// 	nil,
-	// 	nil,
-	// 	// &repo.Pageable{
-	// 	// 	PageSize: 1,
-	// 	// },
-	// 	// &repo.Sortable{
-	// 	// 	SortBy:    "created_at",
-	// 	// 	Direction: 1,
-	// 	// },
-	// )
-	// log.Infof("user %+v", user)
-	// log.Infof("error %+v", err)
-
-	// app := fiber.New(fiber.Config{
-	// 	ServerHeader:             cfg.App.Name,
-	// 	AppName:                  cfg.App.Name,
-	// 	CaseSensitive:            false,
-	// 	StrictRouting:            true,
-	// 	BodyLimit:                4 * 1024 * 1024,
-	// 	Concurrency:              256 * 1024,
-	// 	EnablePrintRoutes:        true,
-	// 	EnableSplittingOnParsers: true,
-	// 	WriteTimeout:             time.Second * 60,
-	// 	ReadTimeout:              time.Second * 60,
-	// })
-
-	// app.All("/", func(c *fiber.Ctx) error {
-	// 	result := db.QueryRow("SELECT current_timestamp")
-	// 	var res any
-	// 	result.Scan(&res)
-	// 	return c.JSON(map[string]any{
-	// 		"message": "Hello, World!",
-	// 		"result":  res,
-	// 	})
-	// })
-
-	// app.All("/api", func(c *fiber.Ctx) error {
-	// 	return c.SendString("API Hello, World!")
-	// })
-
-	// log.Fatal(app.Listen(":" + string(cfg.App.Port)))
+	log.Fatal(app.Listen(":" + string(cfg.App.Port)))
 }
--- backend/go.mod
+++ backend/go.mod
@@ -5,14 +5,20 @@ go 1.23.4
 require golang.org/x/crypto v0.32.0
 
 require (
+	github.com/andybalholm/brotli v1.1.0 // indirect
 	github.com/boombuler/barcode v1.0.1-0.20190219062509-6c824513bacc // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/puzpuzpuz/xsync/v3 v3.5.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
@@ -21,6 +27,9 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	go.opentelemetry.io/otel v1.34.0 // indirect
@@ -34,6 +43,7 @@ require (
 )
 
 require (
+	github.com/gofiber/fiber/v2 v2.52.6
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/google/uuid v1.6.0
