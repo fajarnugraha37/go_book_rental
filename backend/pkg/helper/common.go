cd1f04a53512d15ad3362130212d447c7a1e16dd
fajar <nugrahafajar37@gmail.com>
2025-02-15 21:22:35 +0700

    add util related to panic

--- backend/pkg/helper/common.go
+++ backend/pkg/helper/common.go
@@ -3,3 +3,20 @@ package helper
 func ToPtr[T any](t T) *T {
 	return &t
 }
+
+func Must[T any](runner func() (T, error)) T {
+	result, err := runner()
+	if err != nil {
+		panic(err)
+	}
+
+	return result
+}
+
+func ReturnTuple[T any](result *T, err error) (*T, error) {
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
